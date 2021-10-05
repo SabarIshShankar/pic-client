680fabf6ec43fef2d08a7c9bac33f04d436a42f9
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-10-05 15:01:22 +0530

    u

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,4 +8,46 @@ import logo from "../assets/logo.png";
 
 export const FormWrapper = styled.div`
   background-color: ${(props) => props.theme.white};
+  padding: 1rem;
+  width: 350px;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  margin: 6rem auto;
+  text-align: center;
+  padding: 2rem 0;
+
+  img {
+    margin-bottom: 1.5rem;
+  }
+
+  input {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1rem;
+    padding: 0.5rem 1.2rem;
+    background: ${(props) => props.theme.white};
+    border: 1px solid ${(props) => props.theme.borderColor};
+    font-size: 1rem;
+    border-radius: 4px;
+    font-family: Inter;
+    width: 85%;
+  }
+  input[type="submit"] {
+    background-color: ${(props) => props.theme.blue};
+    color: ${(props) => props.theme.white};
+    border: 1px solid ${(props) => props.theme.blue};
+    cursor: pointer;
+  }
+  p {
+    margin-top: 2rem;
+  }
+  span {
+    color: ${(props) => props.theme.blue};
+    cursor: pointer;
+  }
 `;
+
+const Login = ({ signup }) => {
+  const { setUser } = useContext(UserContext);
+  const email = useInput("");
+  const password = useInput("");
+};
