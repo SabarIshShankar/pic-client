6ed9b06d8d78ddf04ae4a48fb774d56cdd236c3b
sabarIshs19 <66781581+sabarIshs19@users.noreply.github.com>
2021-10-06 17:16:18 +0530

    Create Comment.js

---
+++ src/components/Comment.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+import styled from 'styled-components';
+import Avatar from '../styles/Avatar';
+
+const CommentWrapper = styled.div`
+    display: flex;
+
+    span{
+        padding-right: 0.4rem;
+    }
+`;
+
+const Comment = ({comment, hideAvatar}) => {
+    const history = useHistory();
+
+    return(
+        <CommentWrapper
+        style={{padding: !hideavatar ? "0.4rem 0" : ""}}
+        >
+            {hideavatar && (
+                <Avatar className="pointer"
+                onClick={() => history.push(`/${comment.user.username}`)}
+                src={comment.user.avatar} lat="Avatar"
+                />
+            )}
+
+            <p>
+                <span onClick={() => history.push(`/${comment.user.username}`)}
+                className="bold pointer">{comment.user.username}</span>
+            {comment.text}
+            </p>
+        </CommentWrapper>
+    );
+};
+
+export default Comment;
