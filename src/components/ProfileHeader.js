f62319feb5a0a3b08a3a46223a908ad6206009d0
sabarIshs19 <66781581+sabarIshs19@users.noreply.github.com>
2021-10-10 22:39:38 +0530

    Update ProfileHeader.js

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -104,10 +104,47 @@ const Wrapper = styled.div`
     }
 `;
 
-const modalHeaderStyled = {
+const modalHeaderStyle = {
     display: "flex",
     alignItems: "center",
     justifyContent: "space-between",
     borderBotton: "1px solid #DBDBDB",
     padding: "1rem",
 };
+
+const ModalContextWrapper = sytled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    padding: right: 2rem;
+    font-size: 0.9rem;
+    width: 350px;
+
+    img{
+        width: 40px;
+        object-fit: cover;
+        height: 40px;
+        border-radius: 20px;
+        margin-right: 1rem;
+    }
+    .profile-info{
+        display: flex;
+    }
+    span{
+        color: ${(props) => props.theme.secondaryColor};
+    }
+    button{
+        font-size: 0.9rem;
+        position: relative;
+        top: -10px;
+    }
+
+    @media screen and (max-width: 480px){
+        width: 340px;
+    }
+`;
+
+const ModalContent = ({loggedInUser, users, closeModal, title}) => {
+    const history = useHistory();
+}
