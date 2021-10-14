1066b0aad12318f1315545a2ba8e45088cb2f8ce
sabarIshs19 <66781581+sabarIshs19@users.noreply.github.com>
2021-10-14 18:49:17 +0530

    Update EditProfile.js

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -9,6 +9,26 @@ const Wrapper = styled.div`
     background: ${(props) => props.theme.white};
 
     .tabs{
-        
+        border-right: 1px solid ${(props) => props.theme.borderColor};
+        padding: 1rem;
     }
-`
+
+    .profile-form-container{
+        disply: flex;
+        justify-content: center;
+    }
+
+    @media screen and (max-width: 970px){
+        width: 90%;
+    }
+    @media screen and (max-width: 700px){
+        width: 98%;
+    }
+    @media screen and (max-width: 550px){
+        width: 99%;
+    }
+`;
+
+const EditProfile = () => {
+    
+}
