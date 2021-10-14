6e336069e501b631b9c1f6d983f33e39f3212617
sabarIshs19 <66781581+sabarIshs19@users.noreply.github.com>
2021-10-14 18:45:16 +0530

    Create EditProfile.js

---
+++ src/pages/EditProfile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import ProfileForm from '../components/ProfileForm';
+
+const Wrapper = styled.div`
+    width: 930px;
+    border: 1px solid ${(props) => props.theme.borderColor};
+    display: grid;
+    background: ${(props) => props.theme.white};
+
+    .tabs{
+        
+    }
+`
