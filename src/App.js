12b47244a5e09bdba993d1d4c01470dae2356a08
sabarIshs19 <66781581+sabarIshs19@users.noreply.github.com>
2021-10-16 18:14:56 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./styles.css";
+import React,  {useContext} from 'react';
+import "react-toastify/dist/ReactToastify.css";
+import {ThemeProvider as StyledThemeProvider} from 'styled-components';
+import GlobalStyle from './styles/GlobalStyle';
+import Auth from './components/Auth';
+import Routing from "./Routing";
+import { UserContext } from "./context/UserContext";
+import {ThemeContext} from './context/ThemeContext';
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+const App = () => {
+  const {user} = useContext(UserContext);
+  const {theme} = useContext(ThemeContext);
+
+  return(
+    <StyledThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <ToastContainer autoClose={2000} closeButton={false}/>
+      {user ? <Routing/>: <Auth/>}
+    </StyledThemeProvider>
+  )
 }
+
+export default App;
